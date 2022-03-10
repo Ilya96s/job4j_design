3a19705a19aa5315c615c379a065946284f8e786
Ilya96s <i.kaltygin@icloud.com>
2022-03-10 16:12:26 +0300

    2.1.3 List 3. Удалить head в односвязном списке

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,77 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Реализовать метод Delete для односвязного списка
+ * @param <T>
+ * @author Ilya Kaltygin
+ * @version 1.0
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    /**
+     * Метод добавляет элемент в конец списка
+     * @param value добавляемый элемент в список
+     */
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    /**
+     * Метод удаляет первый элемент в списке
+     * @return возвращает удаленный элемент
+     * @throws NoSuchElementException бросает исключение если элемента нет в списке
+     */
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<T> result = head;
+        head = head.next;
+        return result.value;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
