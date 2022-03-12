36ddbf3d70cfba2f1650be0c4244d8c7c239fe29
Ilya96s <i.kaltygin@icloud.com>
2022-03-12 12:23:00 +0300

    2.1.3 List 5. Очередь на двух стеках

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,40 @@
+package ru.job4j.collection;
+
+/**
+ * Необходимо реализовать очередь на двух стеках по принципу FIFO
+ * FIFO - first in first out
+ * @param <T>
+ * @author Ilya Kaltygin
+ * @version 1.0
+ */
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+    private int inSize;
+    private int outSize;
+
+    /**
+     * Метод poll() должен возвращать первое значене и удалять его из коллекции
+     * @return первое значение в коллекции
+     */
+    public T poll() {
+        if (outSize == 0) {
+            while (inSize != 0) {
+                out.push(in.pop());
+                inSize--;
+                outSize++;
+            }
+        }
+        outSize--;
+        return out.pop();
+    }
+
+    /**
+     * Метод помещает значение в конец коллекции(очереди)
+     * @param value помещаемое значение
+     */
+    public void push(T value) {
+        in.push(value);
+        inSize++;
+    }
+}
