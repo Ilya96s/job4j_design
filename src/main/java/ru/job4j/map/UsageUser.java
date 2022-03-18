351d6ad84d350dfd45da1ec6969bff41a03964a1
Ilya96s <i.kaltygin@icloud.com>
2022-03-18 15:19:22 +0300

    2.1.5 Map 2. Без переопределения equals и hashCode

---
+++ src/main/java/ru/job4j/map/UsageUser.java
@@ -0,0 +1,31 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * При добавлении объектов в мапу с помощью метода put() рассчитывается хэш-код для каждого ключа
+ * с помощью метода hashCode(). Для распределения по бакетам вчисляется значение хэш функции и
+ * рассчитывается индекс бакета.
+ * Так как хеш коды у объектов разные, значит 2 объекта попадут в разные бакеты без вызова метода equals() т.к коллизий не возникло.
+ * @author Ilya Kaltygin
+ * @version 1.0
+ */
+public class UsageUser {
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 2,
+                new GregorianCalendar(1994, Calendar.AUGUST, 24));
+        User user2 = new User("Ivan", 2,
+                new GregorianCalendar(1994, Calendar.AUGUST, 24));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+    }
+}
