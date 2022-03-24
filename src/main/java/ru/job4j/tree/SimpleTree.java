7d124e85df9f19b5dcdd1316bdb15db1134f253a
Ilya96s <i.kaltygin@icloud.com>
2022-03-24 21:29:53 +0300

    2.1.6 Tree 1. Создать элементарную структуру дерева

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,65 @@
+package ru.job4j.tree;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+/**
+ * Создать элементарную структуру дерева
+ * @param <E>
+ * @author  Ilya Kaltygin
+ * @version 1.0
+ */
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    /**
+     * Конструктор принимает в качестве параметра root(корень), т.е при
+     * создании объекта - создается корень дерева и к нему уже с помощью метода add можно
+     * добавлять потомков и т.д
+     * @param root
+     */
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    /**
+     * Метод находит узел по значению parent и добавляет в него дочерний узел child
+     * @param parent Узел-родителя для которого добавляем потомка
+     * @param child Потомок
+     * @return Возвразает true если добавил, иначе false
+     */
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> foundParent = findBy(parent);
+        Optional<Node<E>> foundChild = findBy(child);
+        if (foundParent.isPresent() && foundChild.isEmpty()) {
+           foundParent.get().children.add(new Node<>(child));
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод поиска узла по его значению. Используется алгоритм обхода
+     * дерева в ширину на струткре Queue
+     * @param value Значение для поиска
+     * @return Найденный узел
+     */
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
