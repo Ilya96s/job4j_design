b7f0fcc52152941906ee0e2e32fea14028b34f7b
Ilya96s <i.kaltygin@icloud.com>
2022-04-06 22:09:43 +0300

    2.2.1. Ввод-вывод 0.4. BufferedOutputStream

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,13 +1,22 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Чтение и запись с помощью буферизированных оберток BufferedReader и BufferedOutputStream
+ * @author Ilya Kaltygin
+ * @version 1.0
+ */
 public class LogFilter {
-    public List<String> filter(String file) {
+
+    /**
+     * Метод фильтрует файл используя буферизированный поток
+     * @param file Имя файла
+     * @return Отфильтрованный список
+     */
+    public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader buff = new BufferedReader(new FileReader("log.txt"))) {
             buff.lines()
@@ -19,10 +28,27 @@ public class LogFilter {
         return list;
     }
 
+    /**
+     * Метод принимает список и записывает данные в файл
+     * @param log Список, данные которого необходимо записать
+     * @param file Имя файла
+     */
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String str : log) {
+                out.printf("%s%n", str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter();
-        List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
+        List<String> log = filter("log.txt");
+        save(log, "404.txt");
     }
 }
 
