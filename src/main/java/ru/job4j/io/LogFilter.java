b9b1f481c3045a54e9549c8e92d02b6090e2c612
Ilya96s <i.kaltygin@icloud.com>
2022-04-07 19:29:13 +0300

    2.2.1. Ввод-вывод 0.4. BufferedOutputStream

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -13,10 +13,11 @@ public class LogFilter {
 
     /**
      * Метод фильтрует файл используя буферизированный поток
+     *
      * @param file Имя файла
      * @return Отфильтрованный список
      */
-    public List<String> filter(String file) {
+    public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader buff = new BufferedReader(new FileReader("log.txt"))) {
             buff.lines()
@@ -28,10 +29,28 @@ public class LogFilter {
         return list;
     }
 
+    /**
+     * Метод принимает список и записывает данные в файл
+     *
+     * @param log  Список, данные которого необходимо записать
+     * @param file Имя файла
+     */
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String str : log) {
+                out.printf("%s%n", str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter();
-        List<String> log = logFilter.filter("log.txt");
-        log.forEach(System.out::println);
+        List<String> log = filter("log.txt");
+        save(log, "404.txt");
     }
 }
 
