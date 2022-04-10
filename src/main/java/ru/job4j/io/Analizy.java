e28d379e44f30546f256e72bc11ddf6edef6e203
Ilya96s <i.kaltygin@icloud.com>
2022-04-10 14:04:05 +0300

    2.2.1. Ввод-вывод 2. Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,31 +5,45 @@ import java.io.*;
 /**
  * Анализ доступности сервера
  * @author Ilya Kaltygin
- * @version 1.0
+ * @version 1.1
  */
 public class Analizy {
 
-    StringBuilder sb = new StringBuilder();
+     boolean status = true;
 
     /**
      * Метод находит диапазоны, когда сервер не работал
+     *
      * @param source Имя файла лога
      * @param target Имя результата анализа
      */
     public void unavailable(String source, String target) {
-        try (BufferedReader br = new BufferedReader(new FileReader(source));
-        PrintWriter pr = new PrintWriter(new FileOutputStream(target))) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(source))) {
             br.lines()
                     .forEach(line -> {
-                        if (line.contains("400") || line.contains("500")) {
-                          String[] rsl = line.split(" ");
-                         sb.append(rsl[1]);
+                        if (status && line.contains("400") || line.contains("500")) {
+                            status = false;
+                            String[] array = line.split(" ");
+                            sb.append(array[1])
+                                    .append(";");
+                        }
+                        if (!status && line.contains("200") || line.contains("300")) {
+                            status = true;
+                            String[] array = line.split(" ");
+                            sb.append(array[1])
+                                    .append(";")
+                                    .append(System.lineSeparator());
                         }
                     });
-                    } catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(sb);
+        try (PrintWriter pw = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            pw.println(sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
   
