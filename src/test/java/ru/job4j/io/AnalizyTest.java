b378217cfda17ee3cdbc9a5affb716b11340d048
Ilya96s <i.kaltygin@icloud.com>
2022-04-11 20:37:58 +0300

    2.2.1. Ввод-вывод 3.0. Тестирование IO

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Тестирование IO. Интеграционные тесты
+ * @author Ilya Kaltygin
+ * @version 1.0
+ */
+public class AnalizyTest {
+
+    /**
+     * Для создания файлов во временной директории нужно использовать
+     * класс org.unit.rules.TemporaryFolder.
+     */
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void unavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter pr = new PrintWriter(source)) {
+            pr.println("200 10:56:01");
+            pr.println("200 10:57:01");
+            pr.println("400 10:58:01");
+            pr.println("200 10:59:01");
+        }
+            Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+            br.lines()
+                    .forEach(sb::append);
+        }
+        assertThat(sb.toString(), is("10:58:01;10:59:01;"));
+    }
+}
