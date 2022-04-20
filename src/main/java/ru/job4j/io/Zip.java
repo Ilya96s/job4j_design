80d6a3701459ec842f480481e5bee20431c5b8ad
Ilya96s <i.kaltygin@icloud.com>
2022-04-20 20:22:29 +0300

    2.2.1. Ввод-вывод 5.2. Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -12,9 +12,27 @@ import java.util.zip.ZipOutputStream;
  * Архивировать проект
  * Утилита для архивации папки
  * @author Ilya Kaltygin
- * @version 1.0
+ * @version 1.1
  */
 public class Zip {
+
+    /**
+     * Метод архивирует файлы с помощью аргументов
+     * @param sources Список файлов
+     * @param target Имя и расширение архива
+     */
+    public void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File file : sources) {
+                zip.putNextEntry(new ZipEntry(file.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Метод архивирует файлы с помощью аргументов
@@ -33,30 +51,32 @@ public class Zip {
     }
 
     /**
-     * Метод архивирует файлы с помощью аргументов
-     * @param sources Список файлов
-     * @param target Имя и расширение архива
-     */
-    public void packFiles(List<File> sources, File target) {
-        for (File file : sources) {
-            packSingleFile(file, target);
-        }
-    }
-
-    /**
      * Валидация аргументов
      * @param args Объект с аргементами
      */
-    private static void argumentsValidation(ArgsName args) {
-        if (args.get("d") == null || args.get("e") == null || args.get("o") == null) {
-            throw new IllegalArgumentException("Parameters does not exist");
+    private  void argumentsValidation(ArgsName args) {
+        Path root = Paths.get(args.get("d"));
+        if (!root.toFile().isDirectory()) {
+            throw new IllegalArgumentException("Root is not a directory");
+        }
+        String extension = args.get("e");
+        if (!extension.startsWith(".")) {
+            throw new IllegalArgumentException("Extension should be start with .extension");
+        }
+        Path zip = Paths.get(args.get("o"));
+        String[] nameZip = zip.toFile().getName().split("\\.");
+        if (nameZip.length < 2 || nameZip[1].isEmpty()) {
+            throw new IllegalArgumentException("Archive name should be format: name.extension");
         }
     }
 
     public static void main(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Missing argument, use construction: java -jar pack.jar ARG1 ARG2 ARG3");
+        }
         Zip zip = new Zip();
         ArgsName arguments = ArgsName.of(args);
-        argumentsValidation(arguments);
+        zip.argumentsValidation(arguments);
         List<Path> pathList = Search.search(Paths.get(arguments.get("d")),
                 path -> !path.toFile()
                         .getName()
