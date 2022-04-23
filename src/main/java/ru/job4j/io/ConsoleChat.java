e4e4b3f380a5fb821d5b4bfcfdadc952b504c5ec
Ilya96s <i.kaltygin@icloud.com>
2022-04-23 22:36:48 +0300

    2.2.1. Ввод-вывод 6. Кодировка

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,109 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Работа с кодировкой файлов
+ *
+ * Создание программы консольный чат.
+ * - пользователь вводит слово-фразу, программа берет случайную фразу из текстового файла и выводит в ответ.
+ * - программа замолкает если пользователь вводит слово «стоп», при этом он может продолжать отправлять сообщения в чат.
+ * - если пользователь вводит слово «продолжить», программа снова начинает отвечать.
+ * - при вводе слова «закончить» программа прекращает работу.
+ * - запись диалога, включая слова-команды стоп/продолжить/закончить должны быть записаны в текстовый лог.
+ *
+ * @author Ilya KAltygin
+ * @version 1.0
+ */
+public class ConsoleChat {
+    private static final String OUT = "Закончить";
+    private static final String STOP = "Стоп";
+    private static final String CONTINUE = "Продолжить";
+    private final String path;
+    private final String bootAnswer;
+    List<String> log = new ArrayList<>();
+    List<String> phrase = new ArrayList<>();
+
+    /**
+     * Конструктор
+     * @param path Имя файла в который будет записан весь диалог между пользователем и ботом
+     * @param bootAnswer Имя файла в котором находятся строки с ответами, которые использует бот
+     */
+    public ConsoleChat(String path, String bootAnswer) {
+        this.path = path;
+        this.bootAnswer = bootAnswer;
+    }
+
+    /**
+     * Метод считывает данные с клавиатуры и возвращает случайные ответы.
+     * По окончанию работы метод записывает диалог в текстовый лог
+     */
+    public void run() {
+        boolean chatStatus = true;
+        boolean botStatus = true;
+        phrase = readPhrases();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ввести что то");
+        while (chatStatus) {
+            String line = scanner.nextLine();
+            switch (line) {
+                case STOP -> {
+                    log.add(line);
+                    botStatus = false;
+                }
+                case OUT -> {
+                    log.add(line);
+                    chatStatus = false;
+                }
+               case CONTINUE -> {
+                   botStatus = true;
+               }
+                default -> {
+                    if (botStatus) {
+                        String answer = phrase.get((int) (Math.random() * phrase.size()));
+                        log.add(line);
+                        log.add(answer);
+                        System.out.println(answer);
+                    }
+                }
+            }
+        }
+        serverLog(log);
+    }
+
+    /**
+     * Метод считывает фразы бота из файла и сохраняет их в список
+     * @return возвращает список с фразами
+     */
+    private List<String> readPhrases() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(bootAnswer))) {
+            reader.lines()
+                    .forEach(phrase::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return phrase;
+    }
+
+    /**
+     * Метод сохраняет лог чата в файл в кодировке WINDOWS-1251
+     * @param log Лог чата
+     */
+    private void serverLog(List<String> log) {
+        try (PrintWriter writer = new PrintWriter(
+                new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+            log.forEach(writer::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("data/io/log.txt", "data/io/phrases.txt");
+        cc.run();
+    }
+}
