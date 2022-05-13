b66000746921574cd9e9e15708f7fd39a834f6bb
Ilya96s <i.kaltygin@icloud.com>
2022-05-13 19:34:48 +0300

    2.3.1. Create Update Insert 0. Что такое база данных.

--- postgresql/persons.sql
+++ postgresql/persons.sql
@@ -1,13 +1,13 @@
 create table persons(
 	id serial primary key,
-	names text,
+	first_name text,
 	age integer,
 	gender char(1),
 	growth double_precision
 );
-insert into persons(names, age, gender, growth) values('Ivan', 30, 'M', 186.4);
+insert into persons(first_name, age, gender, growth) values('Ivan', 30, 'M', 186.4);
 select * from persons;
-update persons set names = 'Fedor';
+update persons set first_name = 'Fedor';
 select * from persons;
 delete from persons;
 select * from persons;
