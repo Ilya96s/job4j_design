7f639bea349ff750473aa1ed9433d131bb00af39
Ilya96s <i.kaltygin@icloud.com>
2022-05-24 21:40:46 +0300

    2.3.5. JDBC 0.2. PrepareStatement

---
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,88 @@
+package ru.job4j.spammer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Знакомство с интерфейсом PrepareStatement. Создание параметризованных SQL запросов
+ *
+ * @author Ilya Kaltygin
+ * @version 1.0
+ */
+public class ImportDB {
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    /**
+     * Метод считывает данные из файла, создает объекты на основе полученных данных и сохраняет их в список
+     * @return Список объектов типа User
+     * @throws Exception Исключение
+     */
+    public List<User> load() throws Exception {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            String line;
+            while ((line = rd.readLine()) != null) {
+                String[] array = line.split(";", 3);
+                User user = new User(array[0], array[1]);
+                users.add(user);
+            }
+        }
+        return users;
+    }
+
+    /**
+     * Метод принимает на вход список с объектами, создает подключение в базе данных и заносит объекты в базу данных
+     * @param users Список объектов
+     * @throws ClassNotFoundException Исключение
+     * @throws SQLException Исключение
+     */
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users(name, email) values(?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "data/spammer/dump.txt");
+        db.save(db.load());
+    }
+}
