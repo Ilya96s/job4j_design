8046c85e0f35c9c0dec489a9fe55c1d9f55409d6
Ilya96s <i.kaltygin@icloud.com>
2022-06-29 23:04:51 +0300

    2. SQL Select by company

---
+++ postgresql/select_by_company.sql
@@ -0,0 +1,15 @@
+-- имена всех person, которые не состоят в компании с id = 5;
+-- название компании для каждого человека.
+select person.name "Имя сотрудника", company.name as "Название компании"
+from person
+join company
+on person.company_id = company.id
+where company.id != 5;
+
+-- компании с максимальным количеством человек + количество человек в этой компании
+select company.name as "Название компании", count(person.company_id) as "Количество сотрудников"
+from company
+join person
+on company.id = person.company_id
+group by company.name
+order by count(person.company_id) desc limit 1;
