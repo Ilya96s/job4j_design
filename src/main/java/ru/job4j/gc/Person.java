fd412df7f3819d09e566d6cc69165ecb73f3e0e9
Ilya96s <i.kaltygin@icloud.com>
2022-07-02 17:39:10 +0300

    2.4.1. Понятие сборщик мусора 1. Демонстрация работы GC

---
+++ src/main/java/ru/job4j/gc/Person.java
@@ -0,0 +1,17 @@
+package ru.job4j.gc;
+
+public class Person {
+
+    private int age;
+    private String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", age, name);
+    }
+}
