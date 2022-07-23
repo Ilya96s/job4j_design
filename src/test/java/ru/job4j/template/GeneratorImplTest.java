fb9cd281d08470f767ab508698a75d4cf044c0a1
Ilya96s <i.kaltygin@icloud.com>
2022-07-23 21:58:08 +0300

    2.5.0. TDD 3. Шаблонизатор.

---
+++ src/test/java/ru/job4j/template/GeneratorImplTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Шаблонизатор
+ * Генератор получает шаблон вида "I am a ${name}, Who are ${subject}?"
+ * Слова записанные в фигурных скобках ${..} - это ключи, которые нужно заменить.
+ * В генератор передаются значения этих ключей в виде пары ключ - значение.
+ * Генератор должен вернуть строку: "I am a Petr Arsentev, Who are you?"
+ *  Программа должна учитывать, что в шаблоне есть ключи, которых нет в карте и кидать исключение.
+ *  Программа должна учитывать, что в карте есть лишние ключи и тоже кидать исключение.
+ *
+ * @author Ilya Kaltygin
+ */
+public class GeneratorImplTest {
+
+    @Ignore
+    @Test
+    public void whenAllCorrect() {
+        Generator generator = new GeneratorImpl();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "you");
+        String template = "I am a ${name}, Who are ${subject}?";
+        String expected = "I am a Ivan, Who are you?";
+        String result = generator.produce(template, map);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * В шаблоне есть ключи, которых нет в карте
+     */
+    @Ignore
+    @Test
+    public void whenKeysAreNotIntTheMap() {
+        Generator generator = new GeneratorImpl();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "you");
+        String template = "I am a ${names}, Who are ${subj}?";
+        generator.produce(template, map);
+    }
+
+    /**
+     * В карте лишние ключи
+     */
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExtraKeysInTheMap() {
+        Generator generator = new GeneratorImpl();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivan");
+        map.put("subject", "you");
+        map.put("subj", "Example");
+        String template = "I am a ${name}, Who are ${subject}?";
+        generator.produce(template, map);
+    }
+}
