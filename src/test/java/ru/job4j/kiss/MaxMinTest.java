20a4efca3f8dae92411d6d5c6a36cc3657b1cf1b
Ilya96s <i.kaltygin@icloud.com>
2022-07-23 15:43:50 +0300

    2.5.0. TDD 1. Принципы Kiss, Dry и Yagni

--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.kiss;
 
-
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -17,15 +15,10 @@ import java.util.List;
  * @author Ilya Kaltygin
  */
 public class MaxMinTest {
-    private List<Integer> list;
-
-    @Before
-    public void listCreation() {
-        list = List.of(1, 7, 5, 9, 4, 0, 3, 6, 5);
-    }
 
     @Test
     public void whenMaxIsNine() {
+        List<Integer> list = List.of(4, 6, 2, 0, 9, 7);
         int expected = 9;
         int result = new MaxMin().max(list, Comparator.naturalOrder());
         assertThat(result).isEqualTo(expected);
@@ -33,6 +26,7 @@ public class MaxMinTest {
 
     @Test
     public void whenMinIs0() {
+        List<Integer> list = List.of(4, 6, 2, 0, 9, 7);
         int expected = 0;
         int result = new MaxMin().min(list, Comparator.naturalOrder());
         assertThat(result).isEqualTo(expected);
