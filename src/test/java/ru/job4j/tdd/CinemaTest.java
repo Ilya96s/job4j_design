c1fe45efc5f5a82d7263ea9e637da5b93adf8a60
Ilya96s <i.kaltygin@icloud.com>
2022-07-23 21:51:58 +0300

    2.5.0. TDD 2. Что такое TDD?

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,13 +1,15 @@
 package ru.job4j.tdd;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
 
 /**
  * Что такое TDD?
@@ -21,47 +23,47 @@ public class CinemaTest {
      *
      * Покупка билета
      */
+    @Ignore
     @Test
-    @Disabled
     public void whenBuy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 10, 23, 0);
+        date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertThat(ticket).isEqualTo(new Ticket3D());
+        assertThat(ticket, is(new Ticket3D()));
     }
 
     /**
      * Поиск сеанса
      */
+    @Ignore
     @Test
-    @Disabled
     public void whenFind() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions).isEqualTo(Arrays.asList(new Session3D()));
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
     /**
      * Нет подходящего сеанса
      */
+    @Ignore
     @Test
-    @Disabled
     public void whenNotFind() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> false);
-        assertThat(sessions).isNull();
+        assertThat(sessions, is(new ArrayList<>()));
     }
 
     /**
      * Место занято
      */
-    @Test
-    @Disabled
-    public void whenIncorrectPlace() {
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenSeatIsTaken() {
         Account account = new AccountCinema();
         Account account1 = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -69,20 +71,32 @@ public class CinemaTest {
         date.set(2020, 10, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         Ticket ticket1 = cinema.buy(account1, 1, 1, date);
-        assertThat(ticket1).isNull();
     }
 
     /**
      * Доступных билетов на выбранную дату нет
      */
-    @Test
-    @Disabled
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
     public void whenIncorrectDate() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertThat(ticket).isNull();
     }
+
+    /**
+     * Невалидное место
+     */
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenIncorrectPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 0, 0, date);
+    }
+
 }
