7b0493101e0c9b2078923701327e34972cec8ac8
Ilya96s <i.kaltygin@icloud.com>
2022-07-30 11:36:14 +0300

    2.5.2. OCP 0. Принцип открытости закрытости

--- src/main/java/ru/job4j/design/ocp/ExampleThree.java
+++ src/main/java/ru/job4j/design/ocp/ExampleThree.java
@@ -1,23 +1,20 @@
 package ru.job4j.design.ocp;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Пример № 3
  *
- * Нарушение принципа OCP заключается в том, что поле list не является абстракцией, а представляет собой реализацию интерфейса List
+ * Нарушение принципа OCP заключается в том, поле list представляет конкретную реализацию. Так же вместе типа String нужно использовать
+ * обобщенный тип (WildCard <?>)
  *
  * @author Ilya Kaltygin
  */
 public class ExampleThree {
-    private List<String> list = new ArrayList<>();
 
-    public void add(String s) {
-        list.add(s);
-    }
+    private ArrayList<String> list;
 
-    public void remove(String s) {
-        list.remove(s);
+    public ExampleThree(ArrayList<String> list) {
+        this.list = list;
     }
 }
