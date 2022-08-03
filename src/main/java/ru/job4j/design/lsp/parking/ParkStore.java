0e1820ad5507f129560388d264b241868cb10820
Ilya96s <i.kaltygin@icloud.com>
2022-08-03 22:57:49 +0300

    2.5.3. LSP 2. Парковка машин

---
+++ src/main/java/ru/job4j/design/lsp/parking/ParkStore.java
@@ -0,0 +1,18 @@
+package ru.job4j.design.lsp.parking;
+
+/**
+ * Парковка машин
+ *
+ * Интерфейс парковки. Определяет размер, которое занимает транспортное средство
+ *
+ * @author Ilya Kaltygin
+ */
+public interface ParkStore {
+
+    /**
+     * @param auto Объект типа Auto
+     * @return     Возвращает true, если машина встала на парковку, иначе false;
+     */
+    boolean addAuto(Auto auto);
+
+}
---
+++ src/main/java/ru/job4j/design/lsp/parking/Parking.java
@@ -0,0 +1,20 @@
+package ru.job4j.design.lsp.parking;
+
+/**
+ * Парковка машин
+ *
+ * Класс реализация парковки
+ *
+ * @author Ilya Kaltygin
+ */
+public class Parking implements ParkStore {
+
+    /**
+     * @param auto Объект типа Auto
+     * @return     Возвращает true, если машина встала на парковку, иначе false;
+     */
+    @Override
+    public boolean addAuto(Auto auto) {
+        return false;
+    }
+}
