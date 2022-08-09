b1d24ead8f57e93546ccec8726ea2aa46f06604a
Ilya96s <i.kaltygin@icloud.com>
2022-08-09 23:40:06 +0300

    2.5.4. ISP 1. Создать меню.

---
+++ src/main/java/ru/job4j/design/isp/menu/MenuPrinter.java
@@ -0,0 +1,10 @@
+package ru.job4j.design.isp.menu;
+
+/**
+ * Вывод меню
+ */
+public interface MenuPrinter {
+
+    void print(Menu menu);
+
+}
---
+++ src/main/java/ru/job4j/design/isp/menu/SimpleMenu.java
@@ -0,0 +1,143 @@
+package ru.job4j.design.isp.menu;
+
+import java.util.*;
+
+/**
+ * Каркас меню
+ */
+public class SimpleMenu implements Menu {
+
+    private final List<MenuItem> rootElements = new ArrayList<>();
+
+    @Override
+    public boolean add(String parentName, String childName, ActionDelegate actionDelegate) {
+        return false;
+    }
+
+    @Override
+    public Optional<MenuItemInfo> select(String itemName) {
+        Optional<MenuItemInfo> rsl = Optional.empty();
+        if (findItem(itemName).isPresent()) {
+            rsl = Optional.of(new MenuItemInfo(findItem(itemName).get().menuItem,
+                    findItem(itemName).get().number));
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<MenuItemInfo> iterator() {
+        return new Iterator<>() {
+            DFSIterator dfsIterator = new DFSIterator();
+            @Override
+            public boolean hasNext() {
+                return dfsIterator.hasNext();
+            }
+
+            @Override
+            public MenuItemInfo next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                ItemInfo itemInfo = dfsIterator.next();
+                return new MenuItemInfo(itemInfo.menuItem, itemInfo.number);
+            }
+        };
+    }
+
+    private Optional<ItemInfo> findItem(String name) {
+        Iterator<ItemInfo> iterator = new DFSIterator();
+        Optional<ItemInfo> rsl = Optional.empty();
+        while (iterator.hasNext()) {
+            ItemInfo itemInfo = iterator.next();
+            rsl = Optional.of(itemInfo);
+            break;
+        }
+        return rsl;
+    }
+
+    /**
+     * SimpleMenuItem. Это реализация MenuItem для SimpleMenu
+     */
+    private static class SimpleMenuItem implements MenuItem {
+
+        private String name;
+        private List<MenuItem> children = new ArrayList<>();
+        private ActionDelegate actionDelegate;
+
+        public SimpleMenuItem(String name, ActionDelegate actionDelegate) {
+            this.name = name;
+            this.actionDelegate = actionDelegate;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public List<MenuItem> getChildren() {
+            return children;
+        }
+
+        @Override
+        public ActionDelegate getActionDelegate() {
+            return actionDelegate;
+        }
+    }
+
+    /**
+     * DFSIterator. Это итератор, основанный на поиске в глубину. Но немного модифицированный,
+     * поскольку нам удобно читать пункты меню сверху-вниз и слева-направо.
+     */
+    private class DFSIterator implements Iterator<ItemInfo> {
+
+        Deque<MenuItem> stack = new LinkedList<>();
+
+        Deque<String> numbers = new LinkedList<>();
+
+        DFSIterator() {
+            int number = 1;
+            for (MenuItem item : rootElements) {
+                stack.addLast(item);
+                numbers.addLast(String.valueOf(number++).concat("."));
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public ItemInfo next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            MenuItem current = stack.removeFirst();
+            String lastNumber = numbers.removeFirst();
+            List<MenuItem> children = current.getChildren();
+            int currentNumber = children.size();
+            for (var i = children.listIterator(children.size()); i.hasPrevious();) {
+                stack.addFirst(i.previous());
+                numbers.addFirst(lastNumber.concat(String.valueOf(currentNumber--)).concat("."));
+            }
+            return new ItemInfo(current, lastNumber);
+        }
+
+    }
+
+    /**
+     * ItemInfo. Он служит для того, чтобы скомпоновать пункт меню и номер пункта (1.1., 1.1.1. и т.д.).
+     */
+    private class ItemInfo {
+
+        MenuItem menuItem;
+        String number;
+
+        public ItemInfo(MenuItem menuItem, String number) {
+            this.menuItem = menuItem;
+            this.number = number;
+        }
+    }
+
+}
